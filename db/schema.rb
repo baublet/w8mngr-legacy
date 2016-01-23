40fdbd69150421b10279ce39dd2ef20cf4ca687d
Ryan M. Poe <baublet@gmail.com>
2016-01-23 15:51:35 -0500

    Basic user modeling.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160121164056) do
+ActiveRecord::Schema.define(version: 20160123204132) do
 
   create_table "food_entries", force: :cascade do |t|
     t.string   "description"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160121164056) do
     t.integer  "protein"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
