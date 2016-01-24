6852e621e57e88f009c945a307e5d9d306a6ac1b
Ryan M. Poe <baublet@gmail.com>
2016-01-24 16:58:57 -0500

    Users can now sign up (basics)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123204132) do
+ActiveRecord::Schema.define(version: 20160124175941) do
 
   create_table "food_entries", force: :cascade do |t|
     t.string   "description"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+        @user = User.new(email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    end
 end
