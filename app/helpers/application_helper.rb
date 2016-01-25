4348c46969bf4d588f0ff6ede974ccda959b13c0
Ryan Poe <baublet@gmail.com>
2016-01-25 10:18:09 -0500

    Added some basic CSS rules and the title builder.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    # Returns the page's full title
+    def full_title(page_title = '')
+        base_title = "w8mngr"
+        if page_title.empty?
+            base_title + " | The Lifestyle Tracker"
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
