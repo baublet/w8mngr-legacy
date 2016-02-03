f7df71c54bf80f390ce1c4c2f70bcddf128ea974
Ryan Poe <baublet@gmail.com>
2016-02-03 17:41:18 -0500

    Added a footer, TOS, PP, Contact Form, and further tweaked views.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def privacy_policy
+  end
+
+  def terms_of_service
+  end
+
+  def getting_started
+  end
+
+  def contact_form
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
     root   'welcome#index'
+    get    'privacy'        => 'welcome#privacy_policy'
+    get    'contact'        => 'welcome#contact_form'
+    get    'getting_started'=> 'welcome#getting_started'
+    get    'terms_of_service'=> 'welcome#terms_of_service'
     get    'signup'         => 'users#new'
     get    'login'          => 'sessions#new'
     post   'login'          => 'sessions#create'
     delete 'logout'         => 'sessions#destroy'
     get    'foodlog'        => 'food_entries#index'
     get    '/foodlog/:day'  => 'food_entries#index'
-    resources :food_entries,only: [:index, :create, :update, :destroy]
+    resources :food_entries,
+                            only: [:index, :create, :update, :destroy]
+    get    '/food_entries/delete/:id' =>
+                               'food_entries#destroy',
+                            as: :food_entry_delete
     resources :users
 end
