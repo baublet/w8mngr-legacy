dc873f215a3836fe2805850dcf79fee06689f42e
Ryan Poe <baublet@gmail.com>
2016-02-16 17:30:59 -0500

    Various UX improvements

--- app/helpers/foods_helper.rb
+++ app/helpers/foods_helper.rb
@@ -32,13 +32,13 @@ module FoodsHelper
 
     def make_api_request url
         begin
-            response = open(url).read
+            response = open(url, :read_timeout => 5).read
         rescue OpenURI::HTTPError => e
             response = ActiveSupport::JSON.encode([:error => e.message])
         end
         return ActiveSupport::JSON.decode(response)
     end
-    
+
     # Increments the popularity of food_id and measurement_id by 1
     # Fails silently if one or the other can't be found
     def increment_popularity food_id, measurement_id
