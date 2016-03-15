9c4dadf61d88360d188f0a4822235a66c9d1596e
Ryan Poe <baublet@gmail.com>
2016-03-15 13:12:09 -0400

    Mocked up integration testing for recipes using custom ingredients

---
+++ test/integration/recipes_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class FoodsTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:test)
+		log_in_as(@user)
+		assert logged_in?
+    @recipe_id = 0
+	end
+
+  # Happy path first
+	test "user can create a recipe" do
+		create_valid_recipe
+	end
+
+  test "user can delete a recipe"  do
+    create_valid_recipe
+    delete_button = css_select ".recipe-form .delete-btn"
+    get delete_button[0].href
+    assert_template "recipes/index"
+  end
+
+  test "user can add and remove a custom ingredient to a recipe" do
+    create_valid_recipe
+    post recipes_path, {
+						 :ingredient =>
+						 		{
+									name: "Custom ingredient name",
+									calories: 123,
+                  fat: 234,
+                  carbs: 345,
+                  protein: 456,
+                  recipe_id: @recipe_id
+								}
+						 }
+    assert_template "recipes/edit"
+    ingredient_delete = css_select ".recipe-form .ingredient .delete-btn"
+    get ingredient_delete[0].href
+    assert_template "recipes/edit"
+    assert_select ".recipe-form .ingredient", count: 0
+  end
+
+  private
+
+  def create_valid_recipe
+    get new_recipe_path
+		assert_template "recipes/new"
+		post recipes_path, {
+						 :recipe =>
+						 		{
+									name: "Recipe name",
+									description: "This is a description",
+                  instructions: "These are the instructions"
+								}
+						 }
+		assert_template "recipes/edit"
+		assert_select ".error-explanation", false
+    hidden_fields = css_select ".recipe-form input[type=hidden]"
+    @recipe_id = hidden_fields[0].value.to_int
+  end
+
+end
