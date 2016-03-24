8951e3608a54bbcbaca98860ddd89f0d888046ea
Ryan Poe <baublet@gmail.com>
2016-03-24 17:26:24 -0400

    Added amounts to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320235046) do
+ActiveRecord::Schema.define(version: 20160324212422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160320235046) do
     t.integer  "protein"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "amount"
   end
 
   create_table "measurements", force: :cascade do |t|
