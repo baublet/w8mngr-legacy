4c92336c651cb4e482bee4256bfb5326890a657f
Ryan Poe <baublet@gmail.com>
2016-04-09 17:02:49 -0400

    Added sessions controller test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,51 +1,51 @@
 module SessionsHelper
 
-	# Logins in a user
-	def login(user)
-		session[:user_id] = user.id
-	end
+  # Logins in a user
+  def login(user)
+    session[:user_id] = user.id
+  end
 
-	# Remembers a user (cookies and persistent sessions)
-	def remember(user)
-		user.remember
-		cookies.permanent.signed[:user_id] = user.id
-		cookies.permanent[:remember_token] = user.remember_token
-	end
+  # Remembers a user (cookies and persistent sessions)
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
-	# Returns the current logged-in user (if any).
-	def current_user
-		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: user_id)
-		elsif (user_id = cookies.signed['user_id'])
-			user = User.find_by(id: user_id)
-			if user && user.authenticated?(:remember, cookies[:remember_token])
-				login user
-				@current_user = user
-			end
-		end
-	end
+  # Returns the current logged-in user (if any).
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed['user_id'])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        login user
+        @current_user = user
+      end
+    end
+  end
 
-	# Returns true if the user is logged in, false otherwise.
-	def logged_in?
-		!current_user.nil?
-	end
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
 
-	# Forgets a persistent session
-	def forget(user)
-		user.forget
-		cookies.delete(:user_id)
-		cookies.delete(:remember_token)
-	end
+  # Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
-	# Logs out the current user.
-	def logout
-		forget(current_user)
-		session.delete(:user_id)
-		@current_user = nil
-	end
+  # Logs out the current user.
+  def logout
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
-	# Stores the URL trying to be accessed.
-	def store_location
-		session[:forwarding_url] = request.url if request.get?
-	end
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
