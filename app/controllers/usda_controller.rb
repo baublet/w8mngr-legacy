71ddd22608174a067105f62ed45a5a82c1c49d4b
Ryan Poe <baublet@gmail.com>
2016-04-10 10:48:37 -0400

    Abstracted a bit of my foods api so I can integrate it better inegrate it into other areas

--- app/controllers/usda_controller.rb
+++ app/controllers/usda_controller.rb
@@ -16,42 +16,7 @@ class UsdaController < ApplicationController
       name = @result["name"]
       @food = Food.new(user_id: 1, name: name, ndbno: ndbno)
 
-      # Loop through the nutrients and build our measurements
-      measurements = {}
-      @result["nutrients"][0]["measures"].each do |measure|
-          measurements[measure["label"]] = {
-              "unit" => measure["label"],
-              "amount" => measure["qty"],
-              "calories" => 0,
-              "fat" => 0,
-              "carbs" => 0,
-              "protein" => 0
-          }
-      end
-
-      # Calories (always [1])
-      @result["nutrients"][1]["measures"].each do |measure|
-          measurements[measure["label"]]["calories"] += measure["value"].to_i
-      end
-
-      # Fat (always [3])
-      @result["nutrients"][3]["measures"].each do |measure|
-          measurements[measure["label"]]["fat"] += measure["value"].to_i
-      end
-
-      # Carbs (always [4])
-      @result["nutrients"][4]["measures"].each do |measure|
-          measurements[measure["label"]]["carbs"] += measure["value"].to_i
-      end
-
-      # Protein (always [2])
-      @result["nutrients"][2]["measures"].each do |measure|
-          measurements[measure["label"]]["protein"] += measure["value"].to_i
-      end
-
-      measurements.each do |measurement|
-          @food.measurements.new(measurement.second)
-      end
+      @food.populate_from_usda @result
 
       if !@food.save
           # TODO: Log this behavior
--- app/models/food.rb
+++ app/models/food.rb
@@ -26,4 +26,46 @@ class Food < ActiveRecord::Base
                               :dictionary => "english"
                           }
                       }
+
+  # Load the results passed from the USDA API into this object
+  def populate_from_usda result
+
+    # Loop through the nutrients and build our measurements
+    new_measurements = {}
+    result["nutrients"][0]["measures"].each do |measure|
+        new_measurements[measure["label"]] = {
+            "unit" => measure["label"],
+            "amount" => measure["qty"],
+            "calories" => 0,
+            "fat" => 0,
+            "carbs" => 0,
+            "protein" => 0
+        }
+    end
+
+    # Calories (always [1])
+    result["nutrients"][1]["measures"].each do |measure|
+        new_measurements[measure["label"]]["calories"] += measure["value"].to_i
+    end
+
+    # Fat (always [3])
+    result["nutrients"][3]["measures"].each do |measure|
+        new_measurements[measure["label"]]["fat"] += measure["value"].to_i
+    end
+
+    # Carbs (always [4])
+    result["nutrients"][4]["measures"].each do |measure|
+        new_measurements[measure["label"]]["carbs"] += measure["value"].to_i
+    end
+
+    # Protein (always [2])
+    result["nutrients"][2]["measures"].each do |measure|
+        new_measurements[measure["label"]]["protein"] += measure["value"].to_i
+    end
+
+    new_measurements.each do |measurement|
+        self.measurements.new(measurement.second)
+    end
+  end
+
 end
