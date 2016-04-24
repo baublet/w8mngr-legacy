f62f7129c2d000e0bc94c74388f3ead819752789
Ryan M. Poe <rmp23@duke.edu>
2016-04-24 23:28:46 +0000

    Massive configuration for webpack in prod

--- config/webpack/development.config.js
+++ config/webpack/development.config.js
@@ -1,11 +1,19 @@
 var webpack = require('webpack')
+<<<<<<< 725c33ba4083b8ff00ae66e8ee53618bbe55bf17
+=======
+var _ = require('lodash')
+>>>>>>> Massive configuration for webpack in prod
 var config = module.exports = require('./main.config.js')
 
 config.debug = true
 config.displayErrorDetails = true
 config.outputPathinfo = true
+<<<<<<< 725c33ba4083b8ff00ae66e8ee53618bbe55bf17
 config.devtool = 'sourcemap'
 
 config.plugins.push(
   new webpack.optimize.CommonsChunkPlugin('common', 'common-bundle.js')
 );
+=======
+config.devtool = 'sourcemap'
+>>>>>>> Massive configuration for webpack in prod
