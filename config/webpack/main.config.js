7e23eaa01eeff4c41486b2950683c576d36113c6
Ryan M. Poe <rmp23@duke.edu>
2016-04-24 23:53:26 +0000

    ANOTHER attempt at webpack on Heroku. I will get this working ;)

--- config/webpack/main.config.js
+++ config/webpack/main.config.js
@@ -4,8 +4,11 @@ var webpack = require("webpack");
 var config = module.exports = {
   // the base path which will be used to resolve entry points
   context: __dirname,
-  // the main entry point for our application"s frontend JS
+  // the main entry point for our application's frontend JS
   entry: "../../app/frontend/javascripts/entry.js",
+
+  // No plugins by default
+  plugins: [],
 
   // Our Vue configuration
   module: {
