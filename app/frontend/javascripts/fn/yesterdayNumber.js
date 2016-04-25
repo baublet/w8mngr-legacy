25b2ed88c182a401548260ba47f4ae11d0a621f4
Ryan M. Poe <rmp23@duke.edu>
2016-04-25 13:00:58 +0000

    Updated build dependencies for node

--- app/frontend/javascripts/fn/yesterdayNumber.js
+++ app/frontend/javascripts/fn/yesterdayNumber.js
@@ -1,7 +1,8 @@
 // Converts the current day number to the previous day number
 var numberToDate = require("./numberToDate.js")
 
-module.exports = function(num, format = "%Y%m%d") {
+module.exports = function(num, format) {
+  format = format ? format : "%Y%m%d"
   var date = numberToDate(num)
   date.setDate(date.getDate() - 1)
   return date.strftime(format)
