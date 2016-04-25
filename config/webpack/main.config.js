092848231e99667e75bcc225b667833340bfc7e9
Ryan M. Poe <rmp23@duke.edu>
2016-04-25 11:25:50 +0000

    Updated build dependencies for node

--- config/webpack/main.config.js
+++ config/webpack/main.config.js
@@ -5,7 +5,7 @@ var config = module.exports = {
   // the base path which will be used to resolve entry points
   context: __dirname,
   // the main entry point for our application's frontend JS
-  entry: path.join(__dirname, "..", "..", "frontend", "javascripts", "entry.js"),
+  entry: "../../app/frontend/javascripts/entry.js",
 
   // No plugins by default
   plugins: [],
@@ -29,7 +29,7 @@ var config = module.exports = {
 
 config.output = {
   // this is our app/assets/javascripts directory, which is part of the Sprockets pipeline
-  path: path.join(__dirname, "..", "..", "app", "assets", "javascripts"),
+  path: "../../app/assets/javascripts/",
   // the filename of the compiled bundle, e.g. app/assets/javascripts/bundle.js
   filename: "public-bundle.js",
   // if the webpack code-splitting feature is enabled, this is the path it"ll use to download bundles
