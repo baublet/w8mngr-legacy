be8c29d2d4c3bc8408498f701af935c678dbdc9b
Ryan M. Poe <rmp23@duke.edu>
2016-05-02 16:36:36 +0000

    Add food entries personal trainer file and test

--- lib/modules/personal_trainer/food_entries.rb
+++ lib/modules/personal_trainer/food_entries.rb
@@ -1,6 +1,4 @@
-require 'date'
-# We need this for our date helpers
-require 'active_support/core_ext'
+include ActionView::Helpers::DateHelper
 
 module PersonalTrainer
   module FoodEntries
@@ -12,15 +10,16 @@ module PersonalTrainer
     # Params:
     # +last_entry_date+:: an SQL timestamp string of the last entered item
     # +hours_til_bug+:: an integer of hours before we begin bugging the user (default: 24)
-    def last_entry (last_entry_date, hours_til_bug = 24)
+    def self.last_entry (last_entry_date, hours_til_bug = 24)
+      last_entry_date_s = last_entry_date.to_s
       current_time = DateTime.now
-      last_entry_modified = DateTime.parse(last_entry_date) + hours_til_bug.hours
-      return [] if current_time > last_entry_modified
+      last_entry_modified = DateTime.parse(last_entry_date_s) + hours_til_bug.hours
+      return [] if current_time < last_entry_modified
       # Here, it has been more than hours_til_bug hours since they entered an
       # entry, so let's see how many hours/days it has been and parse that into
       # our message
       type = "food_log_reminder"
-      uid = "flr-" + last_entry_date
+      uid = "flr-" + last_entry_date_s
       time_ago = time_ago_in_words(last_entry_date)
       message_text = "You have not entered any foods into your food log in " + time_ago + "."
       # Then return it!
