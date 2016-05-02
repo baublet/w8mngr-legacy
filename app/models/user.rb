4dd3e528fed869817b12f35657ae4b4efca40a47
Ryan M. Poe <rmp23@duke.edu>
2016-05-02 20:51:22 +0000

    Separate user preferences concern to slim down user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,26 +23,7 @@ class User < ActiveRecord::Base
 
   attr_accessor  :remember_token, :reset_token
 
-  store_accessor :preferences
-  validates_with UserPreferencesValidator
-  def default_preferences
-    {   "name": "",
-        "sex": "na",
-        "birthday": "",
-        "height": "",
-        "height_display": "",
-        "timezone": "",
-        "units": "i",
-
-        "activity_level": 1,
-        "target_calories": 0,
-
-        "pt_messages_food_log": true
-    }
-  end
-
   before_save { email.downcase! }
-  after_initialize { setup_preferences }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -90,36 +71,7 @@ class User < ActiveRecord::Base
     weightentries.where(day: day) || weightentries.none
   end
 
-  # Returns a string representation of their sex
-  def sex
-    if preferences[:sex] == "m"
-      "Male"
-    elsif preferences[:sex] == "f"
-      "Female"
-    else
-      "Other / Prefer not to disclose"
-    end
-  end
 
-  # Returns the default measurement for the user based on their preferences
-  def unit measurement = "human-mass"
-    case measurement
-    when "human-mass"
-      return (preferences["unit"] == "m")? "kg" : "lb"
-    end
-  end
-
-  # Returns the user's age
-  def age
-    dob = Chronic.parse(preferences["birthday"])
-    now = Time.now.utc.to_date
-      now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
-  end
-
-  # Returns the user's name if it's set, or the email if it isn't
-  def name
-    preferences[:name].blank? ? email : preferences[:name]
-  end
 
   # Returns a hash digest of the given string
   def User.digest(string)
@@ -168,17 +120,8 @@ class User < ActiveRecord::Base
     return reset_sent_at < 2.hours.ago
   end
 
-  # Sets up the user preferences so that it doesn't blow up if they haven't yet set values
-  def setup_preferences
-    defaults = default_preferences
-    defaults.each do |pref, default|
-      if preferences.try(:[], pref).nil?
-        preferences[pref.to_s] = default
-      end
-    end
-  end
-
   # Returns the user's avatar image
+  # TODO
   def avatar
     false
   end
