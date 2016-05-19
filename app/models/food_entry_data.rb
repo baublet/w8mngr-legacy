e7b1fb5d71eba9af221cae05c2f908590c18f09f
Ryan M. Poe <rmp23@duke.edu>
2016-05-19 16:07:43 +0000

    Food entry data model first version seems to be working great!

---
+++ app/models/food_entry_data.rb
@@ -0,0 +1,64 @@
+class FoodEntryData
+  include ActiveModel::Model
+
+  attr_accessor(
+    :user_id,
+    :length_scope,
+    :length
+  )
+
+  validates :user_id,       presence: true
+  validates :length,        presence: true,
+                        numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :length_scope,  presence: true,
+                           inclusion: { in: %w(day week month year),
+                             message: "%{value} is not a valid size" }
+
+  # Returns a series of data by either days, weeks, months, or years
+  # depending on the key you pass. It then groups all of the user's
+  # entries by days (adding them all up), and then further groups them
+  # based on the column. If anything other than days, it averages the
+  # days in the designated grouping (week, month, year) and returns
+  # a much smaller hash.
+  #
+  # Returns a hash.
+  #
+  # Params:
+  # +column+:: column you wish to return data from. Values: +:calories, :fat,
+  #            :carbs, :protein+. Default: +:calories+
+  def time_data column = :calories
+    {} if !valid?
+    {} if ![:calories, :fat, :carbs, :protein].include?(column)
+
+    scope_multiplier = 1
+    scope_multiplier = length_scope == "week" ? 7 : scope_multiplier
+    scope_multiplier = length_scope == "month" ? 30 : scope_multiplier
+    scope_multiplier = length_scope == "year" ? 360 : scope_multiplier
+
+    # First, we get the days
+    days = FoodEntry.where(user_id: user_id).group_by_day(:day_ts, default_value: nil, last: length * scope_multiplier).sum(column)
+    days = days.select {|k,v| true if !v.nil?}
+    return days if length_scope == "day"
+
+    # Group by the weeks
+    if length_scope == "week"
+      weeks = days.group_by_week() { |d| d[0] }
+      return Hash[weeks.map { |k,v| [k, v.map(&:last).inject(:+) / v.size] rescue [k, 0] }]
+    end
+
+    # Group by months
+    if length_scope == "month"
+      months = days.group_by_month { |d| d[0] }
+      return Hash[months.map { |k,v| [k, v.map(&:last).inject(:+) / v.size] rescue [k, 0] }]
+    end
+
+    # Group by year
+    if length_scope == "year"
+      years = days.group_by_year { |d| d[0] }
+      return Hash[years.map { |k,v| [k, v.map(&:last).inject(:+) / v.size] rescue [k, 0] }]
+    end
+
+    return {}
+  end
+
+end
--- lib/tasks/charting_tests.rake
+++ lib/tasks/charting_tests.rake
@@ -1,21 +1,37 @@
 
 task :chart => :environment do
   # This will group all of the days in the database
-  days = User.first.foodentries.group_by_day(:day_ts, last: 360).sum(:calories)
-  overall_average = days.map(&:last).inject(:+) / days.size
-  min = overall_average * 0.666
-  puts "Overall average: " + overall_average.to_s
-  # Filter out all underlier days
-  # days = Hash[days.select { |k,v| v > min }]
-  weeks = days.group_by_week() { |d| d[0] }
-  week_averages = Hash[weeks.map { |k,v| [k, v.map(&:last).inject(:+) / v.size] }]
-  #week_averages = Hash[week_averages.select { |k,v| v > min }]
-  months = days.group_by_month { |d| d[0] }
-  month_averages = Hash[months.map { |k,v| [k, v.map(&:last).inject(:+) / v.size] }]
-  week_averages.each do |week, average|
+  id = User.first.id
+  data = FoodEntryData.new(user_id: id)
+  data.length_scope = "day"
+  data.length = 30
+  days = data.time_data
+
+  days.each do |day, total|
+    puts "Day of " + day.to_s + " ---> " + total.to_i.to_s
+  end
+
+  data.length_scope = "week"
+  data.length = 12
+  weeks = data.time_data
+
+  weeks.each do |week, average|
     puts "Week of " + week.to_s + " ---> " + average.to_i.to_s
   end
-  month_averages.each do |month,  average|
+
+  data.length_scope = "month"
+  data.length = 6
+  months = data.time_data
+
+  months.each do |month,  average|
     puts "Month of " + month.to_s + " ---> " + average.to_i.to_s
+  end
+
+  data.length_scope = "year"
+  data.length = 3
+  years = data.time_data
+
+  years.each do |year,  average|
+    puts "Year of " + year.to_s + " ---> " + average.to_i.to_s
   end
 end
