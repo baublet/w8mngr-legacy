b3ae1f3ca4f83cb849c105588446f9b1c47b1e0d
Ryan M. Poe <rmp23@duke.edu>
2016-06-06 19:32:55 +0000

    Update api to allow more stats on dashboard with fewer requests

--- webpack/vue/Dashboard/Dashboard.js
+++ webpack/vue/Dashboard/Dashboard.js
@@ -1,12 +1,37 @@
 import LineChart from "../LineChart.vue"
 import WeekInReview from "../WeekInReview.vue"
 import UserStats from "../UserStats.vue"
+require("../../utilities/strftime.js")
 
 export default {
   el: "#dashboard-app",
   events: {
     "hook:ready": function() {
-      console.log("Loaded dashboard...")
+      console.log("Loading dashboard...")
+      // Load our data from the rails app when the component is ready
+      var app = this
+      this.$dispatch('loading')
+      this.$fetch({
+        method: "GET",
+        url: this.$fetchURI.dashboard.week_in_review,
+        onSuccess: function(response) {
+          app.weekCalories = app.MassageData(response.week_calories)
+          app.weekDays = app.FormatDays(app.weekCalories)
+          app.weekFat = app.MassageData(response.week_fat)
+          app.weekCarbs = app.MassageData(response.week_carbs)
+          app.weekProtein = app.MassageData(response.week_protein)
+          app.weekWeights = app.MassageData(response.week_weights)
+          // Iterate through the week averages and convert them to the desired
+          // format. They often come as equations, so we need to compute them
+          app.weekAverages = response.week_averages
+          for (var key in app.weekAverages) {
+            if(isNaN(app.weekAverages[key]) || !app.weekAverages[key]) app.weekAverages[key] = "-"
+            else app.weekAverages[key] = parseInt(app.weekAverages[key], 10).toLocaleString()
+          }
+          app.$dispatch('notLoading')
+          console.log(app)
+        },
+      })
     },
     "loading": function() {
       this.loading = 1
@@ -19,6 +44,13 @@ export default {
   },
   data: {
     loading: 1,
+    weekAverages: {},
+    weekDays: [],
+    weekCalories: [],
+    weekFat: [],
+    weekCarbs: [],
+    weekProtein: [],
+    weekWeights: [],
   },
   components: {
     LineChart,
@@ -26,5 +58,18 @@ export default {
     UserStats,
   },
   methods: {
+    MassageData: function(data) {
+      return data.map(function(d) {
+        return [d[0], (!d[1] ? '-': parseInt(d[1], 10).toLocaleString())]
+      })
+    },
+    FormatDays: function(data) {
+      var app = this
+      return data.map(function(d) {
+        var display = new Date(d[0]).strftime('%A')
+        var url = app.$fetchURI.food_log_day(new Date(d[0]).strftime('%Y%m%d'))
+        return [url, display]
+      })
+    },
   },
 }
--- webpack/vue/WeekInReview/WeekInReview.js
+++ webpack/vue/WeekInReview/WeekInReview.js
@@ -1,60 +1,11 @@
-require("../../utilities/strftime.js")
-
 export default {
   props: [
-    "chart",
+    "weekAverages",
+    "weekDays",
+    "weekCalories",
+    "weekFat",
+    "weekCarbs",
+    "weekProtein",
+    "weekWeights"
   ],
-  data: function() {
-    return {
-      week_averages: {},
-      week_calories: {},
-      week_fat: {},
-      week_carbs: {},
-      week_protein: {},
-      week_weights: {},
-      week_days: {},
-    }
-  },
-  events: {
-    'hook:ready': function() {
-      // Load our data from the rails app when the component is ready
-      var app = this
-      this.$dispatch('loading')
-      this.$fetch({
-        method: "GET",
-        url: this.$fetchURI.dashboard.week_in_review,
-        onSuccess: function(response) {
-          app.week_calories = app.MassageData(response.week_calories)
-          app.week_days = app.FormatDays(app.week_calories)
-          app.week_fat = app.MassageData(response.week_fat)
-          app.week_carbs = app.MassageData(response.week_carbs)
-          app.week_protein = app.MassageData(response.week_protein)
-          app.week_weights = app.MassageData(response.week_weights)
-          // Iterate through the week averages and convert them to the desired
-          // format. They often come as equations, so we need to compute them
-          app.week_averages = response.week_averages
-          for (var key in app.week_averages) {
-            if(isNaN(app.week_averages[key]) || !app.week_averages[key]) app.week_averages[key] = "-"
-            else app.week_averages[key] = parseInt(app.week_averages[key], 10).toLocaleString()
-          }
-          app.$dispatch('notLoading')
-        },
-      })
-    },
-  },
-  methods: {
-    MassageData: function(data) {
-      return data.map(function(d) {
-        return [d[0], (!d[1] ? '-': parseInt(d[1], 10).toLocaleString())]
-      })
-    },
-    FormatDays: function(data) {
-      var app = this
-      return data.map(function(d) {
-        var display = new Date(d[0]).strftime('%A')
-        var url = app.$fetchURI.food_log_day(new Date(d[0]).strftime('%Y%m%d'))
-        return [url, display]
-      })
-    },
-  },
 }
