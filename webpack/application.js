4c7d8cfaac152e6431d095e2e0817b59df89fdd0
Ryan M. Poe <rmp23@duke.edu>
2016-06-14 21:32:08 +0000

    Fix forEach polyfill bug

--- webpack/application.js
+++ webpack/application.js
@@ -1,3 +1,12 @@
+// Our small forEach polyfill
+if (typeof Array.prototype.forEach !== 'function') {
+  Array.prototype.forEach = function(callback, context) {
+    for (var i = 0; i < this.length; i++) {
+      callback.apply(context, [ this[i], i, this ])
+    }
+  }
+}
+
 // Our App entry point
 var w8mngr = require("w8mngr")
 console.log("w8mngr configuration loaded...")
