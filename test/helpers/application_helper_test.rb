73f7b5ae2558285d851a6825bf5d7fe8d35ca66d
Ryan M. Poe <baublet@gmail.com>
2016-06-14 20:16:15 -0400

    More tests (#25)
    * Add weight_entries_data_controller test
    
    * Add the test! (woops) and expand food entries data controller test
    
    * Add food entry data (model) test
    
    * Add food entry data (model) test
    
    * Add weight entry data(model) test
    
    * Add support for codeclimate test coverage
    
    * Add CC Travis-CI config to .travis.yml
    
    * Add weight_entries message and test for it
    
    * Update rails to 4.2.6
    
    * Add capybara support to gemfile
    
    * Update test and gemfil with beginnings of functional JS testing using Capybara
    
    * Deprecate custom forEach.js function for tested polyfill via babel
    
    * Fix for #23
    
    * Fix renamed method previousAutocompleteItem to prevAutocompleteItem
    
    * Fix broken TCI test related to ruby new lines and string concat
    
    * Update repo with codeclimate configs
    
    * Add application_helper test
    
    * Fix forEach polyfill bug

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,21 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-  # We no longer use days of the week function. Removing this test and
-  # the function
+  test "full_title should be correct" do
+    title = full_title
+    assert_not_empty title
+    assert title.is_a?(String)
+    title = full_title "this goes in the full title"
+    assert_not_empty title
+    assert title.is_a?(String)
+    assert title.include?("this goes in the full title")
+  end
+
+  test "current_day should be correct" do
+    today = current_day
+    assert_not_empty today
+    assert today.is_a?(String)
+    assert today.to_i > 19850101 && today.to_i < 20850101
+  end
 
 end
