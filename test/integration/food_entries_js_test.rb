a343cfa33b773c25a4594ae9f787d60cc48c0cad
Ryan M. Poe <rmp23@duke.edu>
2016-06-27 16:10:26 +0000

    Add delete test to food entries JS test

--- test/integration/food_entries_js_test.rb
+++ test/integration/food_entries_js_test.rb
@@ -23,24 +23,22 @@ class FoodEntriesJSTest < ActionDispatch::IntegrationTest
 
   test "user can add entry to food log" do
     log_in
-    visit foodlog_path
-    assert_equal foodlog_path, current_path
-    within(".app-form") do
-      original = FoodEntry.count
-      fill_in "Description", with: "Test Item"
-      click_button "New Entry"
-      # Why does this test only work reliably when we click this twice? o_O
-      click_button "New Entry"
-      # We check this 15 times after 1 second of waiting since we have to
-      # wait for the browser to send the information, the mocked server to
-      # receive it, and then for the FoodEntry count to be updated. It takes
-      # around 3 seconds...
-      5.times do
-        sleep 1
-        break unless original == FoodEntry.count
-      end
-      assert_not_equal original, FoodEntry.count
+    add_food
+  end
+
+  test "user can delete entry from food log" do
+    log_in
+    add_food
+    original = FoodEntry.count
+    original_shown = find_all(".app-form .row.entry").count
+    first(".delete-btn").click
+    5.times do
+      sleep 1
+      break unless original == FoodEntry.count
     end
+    assert_not_equal original, FoodEntry.count
+    # Makes sure that the item actually disappears from the dom
+    assert_not_equal original_shown, find_all(".app-form .row.entry").count
   end
 
   # Reset sessions and driver between tests
@@ -60,5 +58,30 @@ class FoodEntriesJSTest < ActionDispatch::IntegrationTest
       click_link_or_button "Log in"
     end
     assert_equal dashboard_path, current_path
+  end
+
+  def add_food
+    visit foodlog_path
+    assert_equal foodlog_path, current_path
+    within(".app-form.new") do
+      original = FoodEntry.count
+      fill_in "Description", with: "Test Item"
+      click_button "New Entry"
+      # Why does this test only work reliably when we click this twice? o_O
+      click_button "New Entry"
+      # We check this 15 times after 1 second of waiting since we have to
+      # wait for the browser to send the information, the mocked server to
+      # receive it, and then for the FoodEntry count to be updated. It takes
+      # around 3 seconds...
+      5.times do
+        sleep 1
+        break unless original == FoodEntry.count
+      end
+      assert_not_equal original, FoodEntry.count
+    end
+    # Make sure the new item shows
+    # We have a minimum here of 1 because there are going to be a bunch of these
+    # because each test here adds one...
+    assert_selector ".app-form .row.entry", minimum: 1
   end
 end
