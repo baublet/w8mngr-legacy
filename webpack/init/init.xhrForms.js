edea7ca8740b87a03a7dc1cbb251f35b35c915d2
Ryan M. Poe <rmp23@duke.edu>
2016-07-15 13:26:58 +0000

    Fix bug related to muscle groups form

--- webpack/init/init.xhrForms.js
+++ webpack/init/init.xhrForms.js
@@ -57,15 +57,23 @@ w8mngr.init.add(function() {
   // http://stackoverflow.com/questions/6990729/simple-ajax-form-using-javascript-no-jquery/26556347#26556347
   function xhrForm(form, callback) {
     var xhr = new XMLHttpRequest()
-    var params = [].filter.call(form.elements, function (el) {return !(el.type in ['checkbox', 'radio']) || el.checked})
-    .filter(function(el) { return !!el.name }) //Nameless elements die.
-    .filter(function(el) { return !el.disabled }) //Disabled elements die.
+    var params = [].filter.call(form.elements, function(el) { return true })
+    .filter(function(el) {
+      if(['checkbox', 'radio'].indexOf(el.type) > -1) {
+        return document.getElementById(el.id).checked
+      } else {
+        return true
+      }
+    })                                            // Unchecked checkboxes die
+    .filter(function(el) { return !!el.name })    // Nameless elements die
+    .filter(function(el) { return !el.disabled }) // Disabled elements die
     .map(function(el) {
         return encodeURIComponent(el.name) + '=' + encodeURIComponent(el.value);
     }).join('&') //Then join all the strings by &
     xhr.open("POST", form.action)
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
     xhr.onload = callback.bind(xhr)
+    console.log("Sending " + params + " to " + form.action)
     xhr.send(params)
   }
 })
